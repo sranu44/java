b372c2dfcaf3e104a0bee165973686d2758872ab
srayeshaanu <srayeshaanu@gmail.com>
2023-08-03 19:05:09 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+// var i = 0;
+// for (i=0; i<5; i++){};
+// console.log(i);
+
+// for (var i = 0; i < 10; i++)
+
+
+var i = 5;
+for (; i < 5; i++) {
+ console.log(i);
+}
+
+// 
+
+
+// var smartphone = { brand: "iPhone", model: "13" };
+// console.log(smartphone.brand)
+
+
+
+
+
+function askForCookies() {
+    // console.log("Granma please give me cookies");
+   }
+
+   function movie(){
+    return "Din-The day";
+    return "Poran";
+    return "Hawa";
+   }
+   console.log(movie());
